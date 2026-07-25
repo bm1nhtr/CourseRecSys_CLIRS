4e58bfa402f008c424e82774d448cde35e73e2a6
LesageArno <arno.lesage1@gmail.com>
2026-07-25 14:04:15 +0200

    Update configurations

--- CourseRecSys_CLIRS_slurmjob.sh
+++ CourseRecSys_CLIRS_slurmjob.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=clirs_ppo_k2
-#SBATCH --output=out/clirs_ppo_k2.stdout
-#SBATCH --error=out/clirs_ppo_k2.stderr
+#SBATCH --job-name=jcrecFair_dqn_k2
+#SBATCH --output=out/jcrecFair_dqn_k2.stdout
+#SBATCH --error=out/jcrecFair_dqn_k2.stderr
 #SBATCH --mem=44G
 #SBATCH --cpus-per-task=6
 #SBATCH --gpus=slice
@@ -12,5 +12,5 @@
 
 eval "$(mamba shell hook --shell bash)"
 mamba activate myenv
-python pipelines/run_pipeline.py --Config Config/run_ppo_k2_clirs.json
+python pipelines/run_pipeline.py --Config Config/run_dqn_k2_jcrecFair.json
 mamba deactivate
