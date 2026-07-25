862958aebaa658b583af8333dbe202f8179863bb
LesageArno <arno.lesage1@gmail.com>
2026-07-25 13:58:28 +0200

    results with dqnk2

--- CourseRecSys_CLIRS_slurmjob.sh
+++ CourseRecSys_CLIRS_slurmjob.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=CourseRecSys_CLIRS
-#SBATCH --output=CourseRecSys_run.stdout
-#SBATCH --error=CourseRecSys_run.stderr
+#SBATCH --job-name=clirs_ppo_k2
+#SBATCH --output=out/clirs_ppo_k2.stdout
+#SBATCH --error=out/clirs_ppo_k2.stderr
 #SBATCH --mem=44G
 #SBATCH --cpus-per-task=6
 #SBATCH --gpus=slice
-#SBATCH --time=24:00:00
+#SBATCH --time=16:00:00
 #SBATCH --mail-user=alesage@i3s.unice.fr
 #SBATCH --mail-type=ALL
 
-
 eval "$(mamba shell hook --shell bash)"
 mamba activate myenv
-python pipelines/run_pipeline.py --Config Config/run_dqn_k2_clirs.json
+python pipelines/run_pipeline.py --Config Config/run_ppo_k2_clirs.json
 mamba deactivate
