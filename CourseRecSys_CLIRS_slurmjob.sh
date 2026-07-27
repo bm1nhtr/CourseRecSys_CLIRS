b80fb7067acec453be9a5d78c48bed6f310c5e45
LesageArno <arno.lesage1@gmail.com>
2026-07-27 09:37:21 +0200

    add clirs and jcrecfair k2 ppo

--- CourseRecSys_CLIRS_slurmjob.sh
+++ CourseRecSys_CLIRS_slurmjob.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=jcrec_dqn_k2
-#SBATCH --output=out/jcrec_dqn_k2.stdout
-#SBATCH --error=out/jcrec_dqn_k2.stderr
+#SBATCH --job-name=jcrecFair_ppo_k2
+#SBATCH --output=out/jcrecFair_ppo_k2.stdout
+#SBATCH --error=out/jcrecFair_ppo_k2.stderr
 #SBATCH --mem=44G
 #SBATCH --cpus-per-task=6
 #SBATCH --gpus=slice
-#SBATCH --time=16:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-user=alesage@i3s.unice.fr
 #SBATCH --mail-type=ALL
 
 eval "$(mamba shell hook --shell bash)"
 mamba activate myenv
-python pipelines/run_pipeline.py --Config Config/run_dqn_k2_jcrec.json
+python pipelines/run_pipeline.py --Config Config/run_ppo_k2_jcrecFair.json
 mamba deactivate
